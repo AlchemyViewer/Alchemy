77f54005811c6bb49a74f78903317afc4ed0507f
Rye Mutt <rye@alchemyviewer.org>
2020-03-10 01:36:59 -0400

    Get version revision number from git commit count

--- indra/cmake/BuildVersion.cmake
+++ indra/cmake/BuildVersion.cmake
@@ -1,6 +1,10 @@
 # -*- cmake -*-
 # Construct the viewer version number based on the indra/VIEWER_VERSION file
 
+option(REVISION_FROM_VCS "Get current revision from vcs" ON)
+
+find_package(Git)
+
 if (NOT DEFINED VIEWER_SHORT_VERSION) # will be true in indra/, false in indra/newview/
     set(VIEWER_VERSION_BASE_FILE "${CMAKE_CURRENT_SOURCE_DIR}/newview/VIEWER_VERSION.txt")
 
@@ -10,44 +14,29 @@ if (NOT DEFINED VIEWER_SHORT_VERSION) # will be true in indra/, false in indra/n
         string(REGEX REPLACE "^[0-9]+\\.([0-9]+)\\.[0-9]+" "\\1" VIEWER_VERSION_MINOR ${VIEWER_SHORT_VERSION})
         string(REGEX REPLACE "^[0-9]+\\.[0-9]+\\.([0-9]+)" "\\1" VIEWER_VERSION_PATCH ${VIEWER_SHORT_VERSION})
 
-        if (DEFINED ENV{revision})
+        if ((NOT REVISION_FROM_VCS) AND DEFINED ENV{revision})
            set(VIEWER_VERSION_REVISION $ENV{revision})
            message(STATUS "Revision (from environment): ${VIEWER_VERSION_REVISION}")
-
-        elseif (DEFINED ENV{AUTOBUILD_BUILD_ID})
+        elseif ((NOT REVISION_FROM_VCS) AND DEFINED ENV{AUTOBUILD_BUILD_ID})
            set(VIEWER_VERSION_REVISION $ENV{AUTOBUILD_BUILD_ID})
            message(STATUS "Revision (from autobuild environment): ${VIEWER_VERSION_REVISION}")
+        elseif (Git_FOUND)
+          execute_process(
+                       COMMAND ${GIT_EXECUTABLE} rev-list HEAD --count
+                       OUTPUT_VARIABLE GIT_REV_LIST_COUNT
+                       WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+                       OUTPUT_STRIP_TRAILING_WHITESPACE
+                       )
 
-        else (DEFINED ENV{revision})
-          find_program(MERCURIAL
-                       NAMES hg
-                       PATHS [HKEY_LOCAL_MACHINE\\Software\\TortoiseHG]
-                       PATH_SUFFIXES Mercurial)
-          mark_as_advanced(MERCURIAL)
-          if (MERCURIAL)
-            execute_process(COMMAND ${MERCURIAL} identify --num --rev tip
-                            WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-                            RESULT_VARIABLE hg_id_result
-                            ERROR_VARIABLE hg_id_error
-                            OUTPUT_VARIABLE VIEWER_VERSION_REVISION
-                            OUTPUT_STRIP_TRAILING_WHITESPACE)
-            if (NOT ${hg_id_result} EQUAL 0)
-              message(SEND_ERROR "Revision number generation failed with output:\n${hg_id_error}")
-            else (NOT ${hg_id_result} EQUAL 0)
-              string(REGEX REPLACE "[^0-9a-f]" "" VIEWER_VERSION_REVISION ${VIEWER_VERSION_REVISION})
-            endif (NOT ${hg_id_result} EQUAL 0)
-            if ("${VIEWER_VERSION_REVISION}" MATCHES "^[0-9]+$")
-              message(STATUS "Revision (from hg) ${VIEWER_VERSION_REVISION}")
-            else ("${VIEWER_VERSION_REVISION}" MATCHES "^[0-9]+$")
-              message(STATUS "Revision not set (repository not found?); using 0")
-              set(VIEWER_VERSION_REVISION 0 )
-            endif ("${VIEWER_VERSION_REVISION}" MATCHES "^[0-9]+$")
-           else (MERCURIAL)
-              message(STATUS "Revision not set: mercurial not found; using 0")
+            if(GIT_REV_LIST_COUNT)
+              set(VIEWER_VERSION_REVISION ${GIT_REV_LIST_COUNT})
+            else(GIT_REV_LIST_COUNT)
               set(VIEWER_VERSION_REVISION 0)
-           endif (MERCURIAL)
-        endif (DEFINED ENV{revision})
-        message(STATUS "Building '${VIEWER_CHANNEL}' Version ${VIEWER_SHORT_VERSION}.${VIEWER_VERSION_REVISION}")
+            endif(GIT_REV_LIST_COUNT)
+        else ()
+          set(VIEWER_VERSION_REVISION 0)
+        endif ()
+        message("Building '${VIEWER_CHANNEL}' Version ${VIEWER_SHORT_VERSION}.${VIEWER_VERSION_REVISION}")
     else ( EXISTS ${VIEWER_VERSION_BASE_FILE} )
         message(SEND_ERROR "Cannot get viewer version from '${VIEWER_VERSION_BASE_FILE}'") 
     endif ( EXISTS ${VIEWER_VERSION_BASE_FILE} )
@@ -66,3 +55,39 @@ if (NOT DEFINED VIEWER_SHORT_VERSION) # will be true in indra/, false in indra/n
         "LLBUILD_CONFIG=\"${CMAKE_BUILD_TYPE}\""
         )
 endif (NOT DEFINED VIEWER_SHORT_VERSION)
+
+if (NOT DEFINED VIEWER_COMMIT_LONG_SHA)
+  if(Git_FOUND)
+    execute_process(COMMAND ${GIT_EXECUTABLE} rev-parse HEAD
+                    OUTPUT_VARIABLE GIT_COMMIT_SHA
+                    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+                    OUTPUT_STRIP_TRAILING_WHITESPACE
+                    )
+
+    if(GIT_COMMIT_SHA)
+      set(VIEWER_COMMIT_LONG_SHA ${GIT_COMMIT_SHA})
+    else()
+      set(VIEWER_COMMIT_LONG_SHA 0)
+    endif()
+  else()
+    set(VIEWER_COMMIT_LONG_SHA 0)
+  endif()
+endif (NOT DEFINED VIEWER_COMMIT_LONG_SHA)
+
+if (NOT DEFINED VIEWER_COMMIT_SHORT_SHA)
+  if(Git_FOUND)
+    execute_process(COMMAND ${GIT_EXECUTABLE} rev-parse --short HEAD
+                    OUTPUT_VARIABLE GIT_COMMIT_SHORT_SHA
+                    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+                    OUTPUT_STRIP_TRAILING_WHITESPACE
+                    )
+
+    if(GIT_COMMIT_SHORT_SHA)
+      set(VIEWER_COMMIT_SHORT_SHA ${GIT_COMMIT_SHORT_SHA})
+    else()
+      set(VIEWER_COMMIT_SHORT_SHA 0)
+    endif()
+  else()
+    set(VIEWER_COMMIT_SHORT_SHA 0)
+  endif()
+endif (NOT DEFINED VIEWER_COMMIT_SHORT_SHA)
