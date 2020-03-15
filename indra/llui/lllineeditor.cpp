4befd6ce23a6beae983a5d45b191c00bf8af1f1c
Rye Mutt <rye@alchemyviewer.org>
2020-03-15 02:03:18 -0400

    Lazy creation for line editor menus to reduce menus in memory

--- indra/llui/lllineeditor.cpp
+++ indra/llui/lllineeditor.cpp
@@ -201,13 +201,6 @@ LLLineEditor::LLLineEditor(const LLLineEditor::Params& p)
 
 	setPrevalidateInput(p.prevalidate_input_callback());
 	setPrevalidate(p.prevalidate_callback());
-
-	llassert(LLMenuGL::sMenuContainer != NULL);
-	LLContextMenu* menu = LLUICtrlFactory::instance().createFromFile<LLContextMenu>
-		("menu_text_editor.xml",
-		 LLMenuGL::sMenuContainer,
-		 LLMenuHolderGL::child_registry_t::instance());
-	setContextMenu(menu);
 }
  
 LLLineEditor::~LLLineEditor()
@@ -220,7 +213,7 @@ LLLineEditor::~LLLineEditor()
 	{
         menu->hide();
     }
-	setContextMenu(NULL);
+	setContextMenu(nullptr);
 
 	// calls onCommit() while LLLineEditor still valid
 	gFocusMgr.releaseFocusIfNeeded( this );
@@ -2623,6 +2616,15 @@ LLWString LLLineEditor::getConvertedText() const
 void LLLineEditor::showContextMenu(S32 x, S32 y)
 {
 	LLContextMenu* menu = static_cast<LLContextMenu*>(mContextMenuHandle.get());
+	if (!menu)
+	{
+		llassert(LLMenuGL::sMenuContainer != NULL);
+		menu = LLUICtrlFactory::instance().createFromFile<LLContextMenu>
+			("menu_text_editor.xml",
+				LLMenuGL::sMenuContainer,
+				LLMenuHolderGL::child_registry_t::instance());
+		setContextMenu(menu);
+	}
 
 	if (menu)
 	{
