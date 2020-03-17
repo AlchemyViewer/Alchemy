52da4aba7e0f97098dd2e0a728953a3553397ac4
Rye Mutt <rye@alchemyviewer.org>
2020-03-17 03:23:21 -0400

    Fix leak of voice client observer in LLConversationView

--- indra/newview/llconversationview.cpp
+++ indra/newview/llconversationview.cpp
@@ -97,6 +97,7 @@ LLConversationViewSession::~LLConversationViewSession()
 	if(LLVoiceClient::instanceExists() && mVoiceClientObserver)
 	{
 		LLVoiceClient::getInstance()->removeObserver(mVoiceClientObserver);
+		delete mVoiceClientObserver;
 	}
 
 	mFlashTimer->unset();
@@ -203,7 +204,7 @@ BOOL LLConversationViewSession::postBuild()
 			mIsInActiveVoiceChannel = true;
 			if(LLVoiceClient::instanceExists())
 			{
-				LLNearbyVoiceClientStatusObserver* mVoiceClientObserver = new LLNearbyVoiceClientStatusObserver(this);
+				mVoiceClientObserver = new LLNearbyVoiceClientStatusObserver(this);
 				LLVoiceClient::getInstance()->addObserver(mVoiceClientObserver);
 			}
 			break;
