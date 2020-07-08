1d981094822e062834930034c41183b73b786f2d
Rye Mutt <rye@alchemyviewer.org>
2020-07-07 23:15:25 -0400

    Maybe fasterer float ops?

--- indra/llmath/llvector4a.h
+++ indra/llmath/llvector4a.h
@@ -60,6 +60,9 @@ public:
 	// Call initClass() at startup to avoid 15,000+ cycle penalties from denormalized numbers
 	static void initClass()
 	{
+		unsigned int current_word = 0;
+		_controlfp_s(&current_word, _DN_FLUSH, _MCW_DN);
+		_MM_SET_DENORMALS_ZERO_MODE(_MM_DENORMALS_ZERO_ON);
 		_MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
 		_MM_SET_ROUNDING_MODE(_MM_ROUND_NEAREST);
 	}
