3d39461d7adb9bdbc7a6bfcb225c0b3620383895
Rye Mutt <rye@alchemyviewer.org>
2020-07-21 11:45:30 -0400

    Add option to disable main loop sleep

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -1492,20 +1492,24 @@ bool LLAppViewer::doFrame()
 
 				display();
 
-				static U64 last_call = 0;
-				if (!gTeleportDisplay || gGLManager.mIsIntel) // SL-10625...throttle early, throttle often with Intel
+				static const LLCachedControl<bool> sleep_main_loop(gSavedSettings, "AlchemyMainLoopSleep", true);
+				if (sleep_main_loop)
 				{
-					// Frame/draw throttling
-					U64 elapsed_time = LLTimer::getTotalTime() - last_call;
-					if (elapsed_time < mMinMicroSecPerFrame)
+					static U64 last_call = 0;
+					if (!gTeleportDisplay || gGLManager.mIsIntel) // SL-10625...throttle early, throttle often with Intel
 					{
-						LL_RECORD_BLOCK_TIME(FTM_SLEEP);
-						// llclamp for when time function gets funky
-						U64 sleep_time = llclamp(mMinMicroSecPerFrame - elapsed_time, (U64)1, (U64)1e6);
-						micro_sleep(sleep_time, 0);
+						// Frame/draw throttling
+						U64 elapsed_time = LLTimer::getTotalTime() - last_call;
+						if (elapsed_time < mMinMicroSecPerFrame)
+						{
+							LL_RECORD_BLOCK_TIME(FTM_SLEEP);
+							// llclamp for when time function gets funky
+							U64 sleep_time = llclamp(mMinMicroSecPerFrame - elapsed_time, (U64)1, (U64)1e6);
+							micro_sleep(sleep_time, 0);
+						}
 					}
+					last_call = LLTimer::getTotalTime();
 				}
-				last_call = LLTimer::getTotalTime();
 
 				pingMainloopTimeout("Main:Snapshot");
 				LLFloaterSnapshot::update(); // take snapshots
