b7626a3cf4cd5c389068f229fc52b3eaa195fab9
Rye Mutt <rye@alchemyviewer.org>
2020-07-27 05:38:36 -0400

    Disabling throwing new behavior in msvc until we fix all the operators

--- indra/cmake/00-Common.cmake
+++ indra/cmake/00-Common.cmake
@@ -145,7 +145,7 @@ if (WINDOWS)
   endif ()
 
   if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
-    #set(GLOBAL_CXX_FLAGS "${GLOBAL_CXX_FLAGS} /Zc:externConstexpr /Zc:referenceBinding /Zc:throwingNew")
+    set(GLOBAL_CXX_FLAGS "${GLOBAL_CXX_FLAGS} /Zc:externConstexpr /Zc:referenceBinding")
   elseif("${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang")
     set(GLOBAL_CXX_FLAGS "${GLOBAL_CXX_FLAGS} /Qvec /Zc:dllexportInlines- /clang:-mprefer-vector-width=128 -fno-strict-aliasing -Wno-ignored-pragma-intrinsic -Wno-unused-local-typedef")
   endif()
