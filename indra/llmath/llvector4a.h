c7b2847e23e820c16aa8bc1f9d458bc9230d0ca3
Rye Mutt <rye@alchemyviewer.org>
2020-08-08 16:37:07 -0400

    Enclose windows-specific code in #if block

--- indra/llmath/llvector4a.h
+++ indra/llmath/llvector4a.h
@@ -60,8 +60,10 @@ public:
 	// Call initClass() at startup to avoid 15,000+ cycle penalties from denormalized numbers
 	static void initClass()
 	{
+#if LL_WINDOWS
 		unsigned int current_word = 0;
 		_controlfp_s(&current_word, _DN_FLUSH, _MCW_DN);
+#endif
 		_MM_SET_DENORMALS_ZERO_MODE(_MM_DENORMALS_ZERO_ON);
 		_MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
 		_MM_SET_ROUNDING_MODE(_MM_ROUND_NEAREST);
