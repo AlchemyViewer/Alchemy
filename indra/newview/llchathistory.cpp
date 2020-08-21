d6e8e1bac18902afcf7be7bc8ea76a5b48e7fc38
Rye Mutt <rye@alchemyviewer.org>
2020-08-21 05:03:28 -0400

    Fix gcc indentation warning

--- indra/newview/llchathistory.cpp
+++ indra/newview/llchathistory.cpp
@@ -1157,8 +1157,10 @@ LLView* LLChatHistory::getHeader(const LLChat& chat,const LLStyle::Params& style
 {
 	LLChatHistoryHeader* header = LLChatHistoryHeader::createInstance(mMessageHeaderFilename);
     if (header)
+	{
         header->setup(chat, style_params, args);
-	return header;
+	}
+    return header;
 }
 
 void LLChatHistory::onClickMoreText()
