6b28eea3d3d6cfaa640199d5df690016051b40e0
Rye Mutt <rye@alchemyviewer.org>
2020-10-05 19:08:57 -0400

    Replace boost::random with absl::random for better random number generation

--- indra/llcommon/llrand.cpp
+++ indra/llcommon/llrand.cpp
@@ -29,93 +29,14 @@
 #include "llrand.h"
 #include "lluuid.h"
 
-/**
- * Through analysis, we have decided that we want to take values which
- * are close enough to 1.0 to map back to 0.0.  We came to this
- * conclusion from noting that:
- *
- * [0.0, 1.0)
- *
- * when scaled to the integer set:
- *
- * [0, 4)
- *
- * there is some value close enough to 1.0 that when multiplying by 4,
- * gets truncated to 4. Therefore:
- *
- * [0,1-eps] => 0
- * [1,2-eps] => 1
- * [2,3-eps] => 2
- * [3,4-eps] => 3
- *
- * So 0 gets uneven distribution if we simply clamp. The actual
- * clamp utilized in this file is to map values out of range back
- * to 0 to restore uniform distribution.
- *
- * Also, for clamping floats when asking for a distribution from
- * [0.0,g) we have determined that for values of g < 0.5, then
- * rand*g=g, which is not the desired result. As above, we clamp to 0
- * to restore uniform distribution.
- */
+#include <absl/random/bit_gen_ref.h>
+#include <absl/random/random.h>
 
-// *NOTE: The system rand implementation is probably not correct.
-#define LL_USE_SYSTEM_RAND 0
-
-#if LL_USE_SYSTEM_RAND
-#include <cstdlib>
-#endif
-
-#if LL_USE_SYSTEM_RAND
-class LLSeedRand
+static absl::BitGenRef tls_bit_gen()
 {
-public:
-	LLSeedRand()
-	{
-#if LL_WINDOWS
-		srand(LLUUID::getRandomSeed());
-#else
-		srand48(LLUUID::getRandomSeed());
-#endif
-	}
-};
-static LLSeedRand sRandomSeeder;
-inline F64 ll_internal_random_double()
-{
-#if LL_WINDOWS
-	return (F64)rand() / (F64)RAND_MAX; 
-#else
-	return drand48();
-#endif
+	thread_local static absl::BitGen tlsBitGen;
+	return tlsBitGen;
 }
-inline F32 ll_internal_random_float()
-{
-#if LL_WINDOWS
-	return (F32)rand() / (F32)RAND_MAX; 
-#else
-	return (F32)drand48();
-#endif
-}
-#else
-static LLRandLagFib2281 gRandomGenerator(LLUUID::getRandomSeed());
-inline F64 ll_internal_random_double()
-{
-	// *HACK: Through experimentation, we have found that dual core
-	// CPUs (or at least multi-threaded processes) seem to
-	// occasionally give an obviously incorrect random number -- like
-	// 5^15 or something. Sooooo, clamp it as described above.
-	F64 rv = gRandomGenerator();
-	if(!((rv >= 0.0) && (rv < 1.0))) return fmod(rv, 1.0);
-	return rv;
-}
-
-inline F32 ll_internal_random_float()
-{
-	// The clamping rules are described above.
-	F32 rv = (F32)gRandomGenerator();
-	if(!((rv >= 0.0f) && (rv < 1.0f))) return fmod(rv, 1.f);
-	return rv;
-}
-#endif
 
 S32 ll_rand()
 {
@@ -124,48 +45,25 @@ S32 ll_rand()
 
 S32 ll_rand(S32 val)
 {
-	// The clamping rules are described above.
-	S32 rv = (S32)(ll_internal_random_double() * val);
-	if(rv == val) return 0;
-	return rv;
+	return absl::Uniform<S32>(absl::IntervalClosedOpen, tls_bit_gen(), 0, val);
 }
 
 F32 ll_frand()
 {
-	return ll_internal_random_float();
+	return absl::Uniform<float>(absl::IntervalClosedOpen, tls_bit_gen(), 0.f, 1.f);
 }
 
 F32 ll_frand(F32 val)
 {
-	// The clamping rules are described above.
-	F32 rv = ll_internal_random_float() * val;
-	if(val > 0)
-	{
-		if(rv >= val) return 0.0f;
-	}
-	else
-	{
-		if(rv <= val) return 0.0f;
-	}
-	return rv;
+	return absl::Uniform<float>(absl::IntervalClosedOpen, tls_bit_gen(), 0.f, val);
 }
 
 F64 ll_drand()
 {
-	return ll_internal_random_double();
+	return absl::Uniform<double>(absl::IntervalClosedOpen, tls_bit_gen(), 0.0, 1.0);
 }
 
 F64 ll_drand(F64 val)
 {
-	// The clamping rules are described above.
-	F64 rv = ll_internal_random_double() * val;
-	if(val > 0)
-	{
-		if(rv >= val) return 0.0;
-	}
-	else
-	{
-		if(rv <= val) return 0.0;
-	}
-	return rv;
+	return absl::Uniform<double>(absl::IntervalClosedOpen, tls_bit_gen(), 0.0, val);
 }
--- indra/llcommon/llrand.h
+++ indra/llcommon/llrand.h
@@ -27,35 +27,6 @@
 #ifndef LL_LLRAND_H
 #define LL_LLRAND_H
 
-#include <boost/random/lagged_fibonacci.hpp>
-#include <boost/random/mersenne_twister.hpp>
-
-/**
- * Use the boost random number generators if you want a stateful
- * random numbers. If you want more random numbers, use the
- * c-functions since they will generate faster/better randomness
- * across the process.
- *
- * I tested some of the boost random engines, and picked a good double
- * generator and a good integer generator. I also took some timings
- * for them on linux using gcc 3.3.5. The harness also did some other
- * fairly trivial operations to try to limit compiler optimizations,
- * so these numbers are only good for relative comparisons.
- *
- * usec/inter		algorithm
- * 0.21				boost::minstd_rand0
- * 0.039			boost:lagged_fibonacci19937
- * 0.036			boost:lagged_fibonacci607
- * 0.44				boost::hellekalek1995
- * 0.44				boost::ecuyer1988
- * 0.042			boost::rand48
- * 0.043			boost::mt11213b
- * 0.028			stdlib random() 
- * 0.05				stdlib lrand48()
- * 0.034			stdlib rand()
- * 0.020			the old & lame LLRand
- */
-
 /**
  *@brief Generate a float from [0, RAND_MAX).
  */
@@ -86,42 +57,4 @@ F64 LL_COMMON_API ll_drand();
  */
 F64 LL_COMMON_API ll_drand(F64 val);
 
-/**
- * @brief typedefs for good boost lagged fibonacci.
- * @see boost::lagged_fibonacci
- *
- * These generators will quickly generate doubles. Note the memory
- * requirements, because they are somewhat high. I chose the smallest
- * one, and one comparable in speed but higher periodicity without
- * outrageous memory requirements.
- * To use:
- *  LLRandLagFib607 foo((U32)time(NULL));
- *  double bar = foo();
- */
-
-typedef boost::lagged_fibonacci607 LLRandLagFib607;
-/**< 
- * lengh of cycle: 2^32,000
- * memory: 607*sizeof(double) (about 5K)
- */
-
-typedef boost::lagged_fibonacci2281 LLRandLagFib2281;
-/**< 
- * lengh of cycle: 2^120,000
- * memory: 2281*sizeof(double) (about 17K)
- */
-
-/**
- * @breif typedefs for a good boost mersenne twister implementation.
- * @see boost::mersenne_twister
- *
- * This fairly quickly generates U32 values
- * To use:
- *  LLRandMT19937 foo((U32)time(NULL));
- *  U32 bar = foo();
- *
- * lengh of cycle: 2^19,937-1
- * memory: about 2496 bytes
- */
-typedef boost::mt11213b LLRandMT19937;
 #endif
