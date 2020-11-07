e27806e1e5530dee99baaf763ca1558e6ab63428
Rye Mutt <rye@alchemyviewer.org>
2020-11-07 02:02:50 -0500

    Fix name of appdata dir on macos

--- indra/llvfs/lldir_mac.cpp
+++ indra/llvfs/lldir_mac.cpp
@@ -64,7 +64,7 @@ LLDir_Mac::LLDir_Mac()
 {
 	mDirDelimiter = "/";
 
-    const std::string     secondLifeString = "SecondLife";
+    const std::string     secondLifeString = "AlchemyNext";
     
     std::string *executablepathstr = getSystemExecutableFolder();
 
