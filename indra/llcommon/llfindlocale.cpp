2af753524eb8729c82fc411dbcd27f38b8523eaf
Rye Mutt <rye@alchemyviewer.org>
2021-04-05 15:24:09 -0400

    This is used on linux

--- indra/llcommon/llfindlocale.cpp
+++ indra/llcommon/llfindlocale.cpp
@@ -139,6 +139,16 @@ accumulate_locstring(const char *str, FL_Locale *l) {
   return 0;
 }
 
+#ifndef WIN32
+static int
+accumulate_env(const char* name, FL_Locale* l) {
+    char* env = getenv(name);
+    if (env) {
+        return accumulate_locstring(env, l);
+    }
+    return 0;
+}
+#endif
 
 static void
 canonise_fl(FL_Locale *l) {
