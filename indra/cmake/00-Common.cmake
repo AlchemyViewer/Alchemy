24813f834c39f3e6cdd06ef0fd227ad54466b561
Rye Mutt <rye@alchemyviewer.org>
2021-05-14 19:55:38 -0400

    Downgrade macos to O3 from Ofast/fast-math as it results in broken render on macos

--- indra/cmake/00-Common.cmake
+++ indra/cmake/00-Common.cmake
@@ -279,8 +279,8 @@ if (DARWIN)
   set(CMAKE_CXX_LINK_FLAGS "-Wl,-headerpad_max_install_names,-search_paths_first")
   set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_CXX_LINK_FLAGS}")
   set(DARWIN_extra_cstar_flags "-gdwarf-2")
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Ofast ${DARWIN_extra_cstar_flags}")
-  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Ofast ${DARWIN_extra_cstar_flags}")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O3 ${DARWIN_extra_cstar_flags}")
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -O3 ${DARWIN_extra_cstar_flags}")
   # NOTE: it's critical that the optimization flag is put in front.
   # NOTE: it's critical to have both CXX_FLAGS and C_FLAGS covered.
 
