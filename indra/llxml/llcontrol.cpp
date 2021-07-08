6ccb3f16c30097d52cb8566c67dc898227e2d8ff
XenHat <commits@xenh.at>
2021-07-08 13:28:03 -0400

    Fixed: Resetting unchanged settings values trigger changed event anyway

--- indra/llxml/llcontrol.cpp
+++ indra/llxml/llcontrol.cpp
@@ -315,7 +315,10 @@ void LLControlVariable::resetToDefault(bool fire_signal)
 		mValues.pop_back();
 	}
 	
-	if(fire_signal) 
+	// don't fire if the value didn't actually change
+	LLSD previous_value = getComparableValue(getValue());
+	bool value_changed = (llsd_compare(originalValue, previous_value) == FALSE);
+	if(fire_signal && value_changed)
 	{
 		firePropertyChanged(originalValue);
 	}
