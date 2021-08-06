6fa416feec1ef8f92e026386369339e6056c16e5
Rye Mutt <rye@alchemyviewer.org>
2021-08-06 03:45:57 -0400

    Fix linux sentry linkage

--- indra/cmake/Sentry.cmake
+++ indra/cmake/Sentry.cmake
@@ -13,9 +13,17 @@ if (USE_SENTRY)
             find_library(SENTRY_LIBRARIES Sentry REQUIRED
                 NO_DEFAULT_PATH PATHS "${ARCH_PREBUILT_DIRS_RELEASE}")
         else ()
+            include(CURL)
+            include(NGHTTP2)
+            include(OpenSSL)
+            include(ZLIB)
             set(SENTRY_LIBRARIES 
                 ${ARCH_PREBUILT_DIRS_RELEASE}/libsentry.a
-                ${ARCH_PREBUILT_DIRS_RELEASE}/libbreakpad_client.a)
+                ${ARCH_PREBUILT_DIRS_RELEASE}/libbreakpad_client.a
+                ${CURL_LIBRARIES}
+                ${NGHTTP2_LIBRARIES}
+                ${OPENSSL_LIBRARIES}
+                ${ZLIB_LIBRARIES})
         endif ()
     else ()
         find_package(Sentry REQUIRED)
