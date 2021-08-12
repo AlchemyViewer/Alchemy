AlchemyViewer/AlchemyNext

Run post deferred geometry in linear color space

indra/newview/app_settings/shaders/class1/deferred/fullbrightShinyF.glsl
@@ -99,7 +99,7 @@ void main()
 
 	color.a = 1.0;
 
-	//color.rgb = linear_to_srgb(color.rgb);
+	color.rgb = linear_to_srgb(color.rgb);
 
 	frag_color = color;
 }

AlchemyViewer/Alchemy

indra/newview/app_settings/shaders/class1/deferred/fullbrightShinyF.glsl
@@ -99,7 +99,7 @@ void main()
 
 	color.a = 1.0;
 
-	//color.rgb = linear_to_srgb(color.rgb);
+	color.rgb = srgb_to_linear(color.rgb);
 
 	frag_color = color;
 }