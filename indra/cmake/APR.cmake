bb90c256141e3c294ae458329e5f517d40e57e8e
Rye Mutt <rye@alchemyviewer.org>
2021-08-20 00:54:08 -0400

    Remove dead dependency

--- indra/cmake/APR.cmake
+++ indra/cmake/APR.cmake
@@ -44,7 +44,6 @@ else (USESYSTEMLIBS)
   set(APR_INCLUDE_DIR ${LIBS_PREBUILT_DIR}/include/apr-1)
 
   if (LINUX)
-      list(APPEND APRUTIL_LIBRARIES uuid)
     list(APPEND APRUTIL_LIBRARIES rt)
   endif (LINUX)
 endif (USESYSTEMLIBS)
