043982bda8a7c90e5a8ad83299c2d76fb031477e
Rye Mutt <rye@alchemyviewer.org>
2022-01-19 10:30:58 -0500

    Change to c-ares for curl asynch dns resolving for improved non-pipeline http performance

--- indra/cmake/CURL.cmake
+++ indra/cmake/CURL.cmake
@@ -8,15 +8,26 @@ set(CURL_FIND_REQUIRED ON)
 if (USESYSTEMLIBS)
   include(FindCURL)
 else (USESYSTEMLIBS)
+  use_prebuilt_binary(c-ares)
   use_prebuilt_binary(curl)
   if (WINDOWS)
     set(CURL_LIBRARIES 
       debug ${ARCH_PREBUILT_DIRS_DEBUG}/libcurld.lib
       optimized ${ARCH_PREBUILT_DIRS_RELEASE}/libcurl.lib
+      debug ${ARCH_PREBUILT_DIRS_DEBUG}/libcaresd.lib
+      optimized ${ARCH_PREBUILT_DIRS_RELEASE}/libcares.lib
       Normaliz.lib
     )
-  else (WINDOWS)
+  elseif(DARWIN)
+    set(CURL_LIBRARIES 
+      debug ${ARCH_PREBUILT_DIRS_DEBUG}/libcurld.a
+      optimized ${ARCH_PREBUILT_DIRS_RELEASE}/libcurl.a
+      debug ${ARCH_PREBUILT_DIRS_DEBUG}/libcares.a
+      optimized ${ARCH_PREBUILT_DIRS_RELEASE}/libcares.a
+      resolv
+    )
+  else ()
     set(CURL_LIBRARIES libcurl.a)
-  endif (WINDOWS)
+  endif ()
   set(CURL_INCLUDE_DIRS ${LIBS_PREBUILT_DIR}/include)
 endif (USESYSTEMLIBS)
