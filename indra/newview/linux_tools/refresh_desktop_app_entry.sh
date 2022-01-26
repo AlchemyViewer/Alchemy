f3a4b9528af29c086837f233bb1165ca18781eae
Xenhat Hex <me@xenh.at>
2022-01-26 14:30:34 -0500

    Additional tweaks to the desktop entry script

--- indra/newview/linux_tools/refresh_desktop_app_entry.sh
+++ indra/newview/linux_tools/refresh_desktop_app_entry.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-SCRIPTSRC=`readlink -f "$0" || echo "$0"`
-RUN_PATH=`dirname "${SCRIPTSRC}" || echo .`
+SCRIPTSRC="$(readlink -f "$0" || echo "$0")"
+RUN_PATH="$(dirname "${SCRIPTSRC}" || echo .)"
 
-install_prefix=${RUN_PATH}/..
+install_prefix="${RUN_PATH}"/..
 
 function install_desktop_entry()
 {
-    local installation_prefix="$1"
-    local desktop_entries_dir="$2"
+    local installation_prefix="${1}"
+    local desktop_entries_dir="${2}"
 
     local desktop_entry="\
 [Desktop Entry]\n\
@@ -25,13 +25,13 @@ X-Desktop-File-Install-Version=3.0"
 
     echo " - Installing menu entries in ${desktop_entries_dir}"
     mkdir -vp "${desktop_entries_dir}"
-    echo -e $desktop_entry > "${desktop_entries_dir}/alchemy-viewer.desktop" || "Failed to install application menu!"
+    echo -e "${desktop_entry}" > "${desktop_entries_dir}/alchemy-viewer.desktop" || "Failed to install application menu!"
 }
 
 if [ "$UID" == "0" ]; then
     # system-wide
-    install_desktop_entry "$install_prefix" /usr/local/share/applications
+    install_desktop_entry "${install_prefix}" /usr/local/share/applications
 else
     # user-specific
-    install_desktop_entry "$install_prefix" "$HOME/.local/share/applications"
+    install_desktop_entry "${install_prefix}" "${HOME}/.local/share/applications"
 fi
