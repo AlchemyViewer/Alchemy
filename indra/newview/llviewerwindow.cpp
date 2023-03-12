d0dba6e4ec852db7c172ef8d7f7f8445968ae73d
Rye Mutt <rye@alchemyviewer.org>
2023-03-11 21:48:01 -0500

    Improve error handling in snapshot code

--- indra/newview/llviewerwindow.cpp
+++ indra/newview/llviewerwindow.cpp
@@ -4859,6 +4859,11 @@ void LLViewerWindow::saveImageLocal(LLImageFormatted *image, const snapshot_save
 	if (ec.failed())
 	{
 		LL_WARNS() << "Failed check for is directory for filesystem path " << lastSnapshotDir << " : " << ec.message() << LL_ENDL;
+		LLSD args;
+		args["PATH"] = lastSnapshotDir;
+		LLNotificationsUtil::add("SnapshotToLocalDirNotExist", args);
+		resetSnapshotLoc();
+		failure_cb();
 		return;
 	}
 	if (!is_dir)
@@ -4874,6 +4879,11 @@ void LLViewerWindow::saveImageLocal(LLImageFormatted *image, const snapshot_save
 	if (ec.failed())
 	{
 		LL_WARNS() << "Failed to read disk space for filesystem path " << lastSnapshotDir << " : " << ec.message() << LL_ENDL;
+		LLSD args;
+		args["PATH"] = lastSnapshotDir;
+		LLNotificationsUtil::add("SnapshotToLocalDirNotExist", args);
+		resetSnapshotLoc();
+		failure_cb();
 		return;
 	}
 	if (b_space.free < image->getDataSize())
