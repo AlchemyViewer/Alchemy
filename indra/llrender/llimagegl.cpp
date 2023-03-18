e3b4815920c8485f85fde1899ac264473bbb862f
Rye Cogtail <rye@alchemyviewer.org>
2023-03-18 11:10:13 -0400

    Fix some GCC warnings

--- indra/llrender/llimagegl.cpp
+++ indra/llrender/llimagegl.cpp
@@ -910,7 +910,7 @@ BOOL LLImageGL::setImage(const U8* data_in, BOOL data_hasmips /* = FALSE */, S32
 						{
 							stop_glerror();
 
-							if (prev_mip_data)
+							if (prev_mip_data != cur_mip_data)
 								delete[] prev_mip_data;
 							if (cur_mip_data)
 								delete[] cur_mip_data;
