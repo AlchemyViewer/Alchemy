aff44f560905bc14cb901beadd7bc854bfb18c97
Rye Mutt <rye@alchemyviewer.org>
2023-04-02 22:57:37 -0400

    Fix opengl lib linking on windows

--- indra/cmake/OpenGL.cmake
+++ indra/cmake/OpenGL.cmake
@@ -4,3 +4,10 @@ include(Variables)
 include(Prebuilt)
 include(FindOpenGL)
 
+add_library( ll::opengl INTERFACE IMPORTED )
+
+if(TARGET OpenGL::OpenGL)
+	target_link_libraries( ll::opengl INTERFACE OpenGL::OpenGL)
+elseif(TARGET OpenGL::GL)
+	target_link_libraries( ll::opengl INTERFACE OpenGL::GL)
+endif()
