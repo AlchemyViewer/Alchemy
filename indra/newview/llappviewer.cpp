e3daf6773463359c57b568afa078763e9500f947
Rye Mutt <rye@alchemyviewer.org>
2023-04-05 23:10:14 -0400

    Bump cache versions to fix a few issues..

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -4239,7 +4239,7 @@ U32 LLAppViewer::getTextureCacheVersion()
 	// 2021-03-10 Bumping up by one to help obviate texture cache issues with
 	//            Simple Cache Viewer - see SL-14985 for more information
 	//const U32 TEXTURE_CACHE_VERSION = 8;
-	const U32 TEXTURE_CACHE_VERSION = 9;
+	const U32 TEXTURE_CACHE_VERSION = 10;
 
 	return TEXTURE_CACHE_VERSION ;
 }
@@ -4258,7 +4258,7 @@ U32 LLAppViewer::getObjectCacheVersion()
 {
 	// Viewer object cache version, change if object update
 	// format changes. JC
-	const U32 INDRA_OBJECT_CACHE_VERSION = 15;
+	const U32 INDRA_OBJECT_CACHE_VERSION = 16;
 
 	return INDRA_OBJECT_CACHE_VERSION;
 }
