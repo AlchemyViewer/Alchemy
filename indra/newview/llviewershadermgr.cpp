21b6dc559790180d933c15c82400deb0cfd71e12
Rye Mutt <rye@alchemyviewer.org>
2023-11-14 12:50:20 -0500

    Disable tonemapping and LPM unless on OGL 4.6

--- indra/newview/llviewershadermgr.cpp
+++ indra/newview/llviewershadermgr.cpp
@@ -2886,7 +2886,34 @@ BOOL LLViewerShaderMgr::loadShadersDeferred()
 	// These shaders are non-critical and do not fail shader load
 	if (success)
 	{
-		if (gGLManager.mGLVersion >= 4.19f)
+		// [RLVa:KB] - @setsphere
+		if (success)
+		{
+			gRlvSphereProgram.mName = "RLVa Sphere Post Processing Shader";
+			gRlvSphereProgram.mFeatures.isDeferred = true;
+			gRlvSphereProgram.mShaderFiles.clear();
+			gRlvSphereProgram.mShaderFiles.push_back(make_pair("deferred/rlvV.glsl", GL_VERTEX_SHADER));
+			if (gGLManager.mGLVersion >= 4.5f)
+				gRlvSphereProgram.mShaderFiles.push_back(make_pair("deferred/rlvF.glsl", GL_FRAGMENT_SHADER));
+			else
+				gRlvSphereProgram.mShaderFiles.push_back(make_pair("deferred/rlvFLegacy.glsl", GL_FRAGMENT_SHADER));
+			gRlvSphereProgram.mShaderLevel = mShaderLevel[SHADER_DEFERRED];
+			success = gRlvSphereProgram.createShader(NULL, NULL);
+		}
+		// [/RLV:KB]
+
+		if (success)
+		{
+			gDeferredPostDLSProgram.mName = "DLS Shader";
+			gDeferredPostDLSProgram.mFeatures.hasSrgb = true;
+			gDeferredPostDLSProgram.mShaderFiles.clear();
+			gDeferredPostDLSProgram.mShaderFiles.push_back(make_pair("alchemy/postNoTCV.glsl", GL_VERTEX_SHADER));
+			gDeferredPostDLSProgram.mShaderFiles.push_back(make_pair("alchemy/DLSF.glsl", GL_FRAGMENT_SHADER));
+			gDeferredPostDLSProgram.mShaderLevel = mShaderLevel[SHADER_DEFERRED];
+			success = gDeferredPostDLSProgram.createShader(NULL, NULL);
+		}
+
+		if (gGLManager.mGLVersion >= 4.59f)
 		{
 			gDeferredPostCASProgram.mName = "Contrast Adaptive Sharpen Shader";
 			gDeferredPostCASProgram.mFeatures.hasSrgb = true;
@@ -2897,17 +2924,7 @@ BOOL LLViewerShaderMgr::loadShadersDeferred()
 			gDeferredPostCASProgram.createShader(NULL, NULL);
 		}
 
-		{
-			gDeferredPostDLSProgram.mName = "DLS Shader";
-			gDeferredPostDLSProgram.mFeatures.hasSrgb = true;
-			gDeferredPostDLSProgram.mShaderFiles.clear();
-			gDeferredPostDLSProgram.mShaderFiles.push_back(make_pair("alchemy/postNoTCV.glsl", GL_VERTEX_SHADER));
-			gDeferredPostDLSProgram.mShaderFiles.push_back(make_pair("alchemy/DLSF.glsl", GL_FRAGMENT_SHADER));
-			gDeferredPostDLSProgram.mShaderLevel = mShaderLevel[SHADER_DEFERRED];
-			gDeferredPostDLSProgram.createShader(NULL, NULL);
-		}
-
-		if (gGLManager.mGLVersion >= 4.19f)
+		if (gGLManager.mGLVersion >= 4.59f)
 		{
 			gDeferredPostTonemapLPMProgram.mName = "Tonemapping Shader LPM";
 			gDeferredPostTonemapLPMProgram.mFeatures.hasSrgb = true;
@@ -2920,21 +2937,6 @@ BOOL LLViewerShaderMgr::loadShadersDeferred()
 			gDeferredPostTonemapLPMProgram.addPermutation("TONEMAP_METHOD", std::to_string(ALRenderUtil::TONEMAP_AMD));
 			gDeferredPostTonemapLPMProgram.createShader(NULL, NULL); // Ignore return value for this shader
 		}
-// [RLVa:KB] - @setsphere
-		if (success)
-		{
-			gRlvSphereProgram.mName = "RLVa Sphere Post Processing Shader";
-			gRlvSphereProgram.mFeatures.isDeferred = true;
-			gRlvSphereProgram.mShaderFiles.clear();
-			gRlvSphereProgram.mShaderFiles.push_back(make_pair("deferred/rlvV.glsl", GL_VERTEX_SHADER));
- 			if (gGLManager.mGLVersion >= 4.5f)
-				gRlvSphereProgram.mShaderFiles.push_back(make_pair("deferred/rlvF.glsl", GL_FRAGMENT_SHADER));
-			else
-				gRlvSphereProgram.mShaderFiles.push_back(make_pair("deferred/rlvFLegacy.glsl", GL_FRAGMENT_SHADER));
-			gRlvSphereProgram.mShaderLevel = mShaderLevel[SHADER_DEFERRED];
-			gRlvSphereProgram.createShader(NULL, NULL);
-		}
-// [/RLV:KB]
 	}
 
 	return success;
