8188f061b698f3e675786042079c88097d6afd46
Rye Mutt <rye@alchemyviewer.org>
2024-03-01 23:15:46 -0500

    Attempt linux freetype

--- indra/cmake/FreeType.cmake
+++ indra/cmake/FreeType.cmake
@@ -4,15 +4,6 @@ include(Prebuilt)
 include_guard()
 add_library( ll::freetype INTERFACE IMPORTED )
 
-if(LINUX)
-    include(FindPkgConfig)
-
-    pkg_check_modules(freetype2 REQUIRED IMPORTED_TARGET freetype2)
-    pkg_check_modules(fontconfig REQUIRED IMPORTED_TARGET fontconfig)
-    target_link_libraries( ll::freetype INTERFACE PkgConfig::freetype2 PkgConfig::fontconfig)
-    return()
-endif()
-
 use_system_binary(freetype)
 use_prebuilt_binary(freetype)
 target_include_directories( ll::freetype SYSTEM INTERFACE  ${LIBS_PREBUILT_DIR}/include/freetype2/)
@@ -21,5 +12,14 @@ if (WINDOWS)
       debug ${ARCH_PREBUILT_DIRS_DEBUG}/freetyped.lib
       optimized ${ARCH_PREBUILT_DIRS_RELEASE}/freetype.lib)
 else()
-    target_link_libraries( ll::freetype INTERFACE freetype)
+    target_link_libraries( ll::freetype INTERFACE
+      debug ${ARCH_PREBUILT_DIRS_DEBUG}/libfreetyped.a
+      optimized ${ARCH_PREBUILT_DIRS_RELEASE}/libfreetype.a)
+endif()
+
+if(LINUX)
+    include(FindPkgConfig)
+
+    pkg_check_modules(fontconfig REQUIRED IMPORTED_TARGET fontconfig)
+    target_link_libraries( ll::freetype INTERFACE PkgConfig::fontconfig)
 endif()
