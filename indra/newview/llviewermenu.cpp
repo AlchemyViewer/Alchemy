08d993268ebafecd72cf69ce6fec91a80d1d7877
Rye <rye@alchemyviewer.org>
2026-03-06 23:34:05 -0500

    Add always run enabled/disabled notification

--- indra/newview/llviewermenu.cpp
+++ indra/newview/llviewermenu.cpp
@@ -6609,11 +6609,13 @@ class LLWorldAlwaysRun : public view_listener_t
         {
             gAgent.clearAlwaysRun();
 //          gAgent.clearRunning();
+            LLNotifications::instance().add("AlwaysRunDisabled" , LLSD(), LLSD());
         }
         else
         {
             gAgent.setAlwaysRun();
 //          gAgent.setRunning();
+            LLNotifications::instance().add("AlwaysRunEnabled", LLSD(), LLSD());
         }
 
         // tell the simulator.
