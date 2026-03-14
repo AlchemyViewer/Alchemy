46fd2768657ee9b69319c2c79dd9171e81602629
Rye <rye@alchemyviewer.org>
2026-03-14 01:16:51 -0400

    Fix udp network stats being off by * 8

--- indra/newview/llviewerregion.cpp
+++ indra/newview/llviewerregion.cpp
@@ -2142,7 +2142,7 @@ void LLViewerRegion::updateNetStats()
     mLastPacketsLost =  mPacketsLost;
 
     mPacketsIn =                cdp->getPacketsIn();
-    mBitsIn =                   8 * cdp->getBytesIn();
+    mBitsIn =                   cdp->getBytesIn();
     mPacketsOut =               cdp->getPacketsOut();
     mPacketsLost =              cdp->getPacketsLost();
     mPingDelay =                cdp->getPingDelay();
