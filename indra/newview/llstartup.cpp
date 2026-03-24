3b9b1ffad27555ff20a6e853f619b2f3b9040043
Rye <rye@alchemyviewer.org>
2026-03-24 06:15:12 -0400

    Reapply "Fix wall of spam during login from various udp handlers not being setup"
    This reverts commit 45d4cb624a7d47a0a9191f4c49d97a31752704bf.

--- indra/newview/llstartup.cpp
+++ indra/newview/llstartup.cpp
@@ -2104,6 +2104,15 @@ bool idle_startup()
             return false;
         }
 
+        // set up callbacks
+        LL_INFOS() << "Registering Callbacks" << LL_ENDL;
+        LLMessageSystem* msg = gMessageSystem;
+        LL_INFOS() << " Inventory" << LL_ENDL;
+        LLInventoryModel::registerCallbacks(msg);
+        LL_INFOS() << " Landmark" << LL_ENDL;
+        LLLandmark::registerCallbacks(msg);
+        do_startup_frame();
+
         LLInventoryModelBackgroundFetch::instance().start();
         LLAppearanceMgr::instance().initCOFID();
         LLUUID cof_id = LLAppearanceMgr::instance().getCOF();
@@ -2125,15 +2134,6 @@ bool idle_startup()
         gInventory.addChangedMask(LLInventoryObserver::ALL, LLUUID::null);
         gInventory.notifyObservers();
 
-        do_startup_frame();
-
-        // set up callbacks
-        LL_INFOS() << "Registering Callbacks" << LL_ENDL;
-        LLMessageSystem* msg = gMessageSystem;
-        LL_INFOS() << " Inventory" << LL_ENDL;
-        LLInventoryModel::registerCallbacks(msg);
-        LL_INFOS() << " Landmark" << LL_ENDL;
-        LLLandmark::registerCallbacks(msg);
         do_startup_frame();
 
         // request all group information
