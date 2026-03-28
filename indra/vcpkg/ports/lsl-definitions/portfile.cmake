8e9ee905c3a381bde2c2ae7aa567d207a2e406fb
Rye <rye@alchemyviewer.org>
2026-03-28 11:21:44 -0400

    Update lsl-definitions package to 0.5.5

--- indra/vcpkg/ports/lsl-definitions/portfile.cmake
+++ indra/vcpkg/ports/lsl-definitions/portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_download_distfile(
     LSL_ARCHIVE
     URLS "https://github.com/secondlife/lsl-definitions/releases/download/v${VERSION}/lsl-definitions.zip"
     FILENAME lsl-definitions.zip
-    SHA512 a5344cc7a945d1208b0a1e27dc5cbe99a2dd28d9836f79e5a760d5df074b0393590a335f743209b7d5dec00fa6e7017aa2dfe7f16b3bf3f5df19cc511eca2958
+    SHA512 56e683ba340b238f535739001718c83f0235a9fc2ce263db521af021c788743e5b880056a95fa7c38ba2a9440a59c2968f286db7d240cd8e84c6bbd91a4937a5
 )
 
 vcpkg_extract_source_archive(LSL_DIR ARCHIVE ${LSL_ARCHIVE} NO_REMOVE_ONE_LEVEL)
@@ -16,4 +16,4 @@ file(INSTALL "${LSL_DIR}/slua_default.docs.json" DESTINATION "${CURRENT_PACKAGES
 file(INSTALL "${LSL_DIR}/slua_keywords_pretty.xml" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}/definitions")
 file(INSTALL "${LSL_DIR}/slua_selene.yml" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}/definitions")
 
-file(TOUCH "${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright")
+vcpkg_install_copyright(FILE_LIST "${LSL_DIR}/LICENSE")
