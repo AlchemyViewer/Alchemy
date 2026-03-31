b1c0b7e562f5746a89933ea107cadf26038dbd91
Rye <rye@alchemyviewer.org>
2026-03-31 09:00:08 -0400

    Fix staging source han sans in vcpkg port

--- indra/vcpkg/ports/alchemy-fonts/portfile.cmake
+++ indra/vcpkg/ports/alchemy-fonts/portfile.cmake
@@ -1,13 +1,24 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO AlchemyViewer/alchemy-fonts
-    REF f9de5abc31a8f5703dd7def2fe994ead4065ce48
-    SHA512 97a9450bea3475bead995483e153e734a9c7204698f0f2f46c153a660d85a8f922d30257b4b782ff0e0db5936fb2b6cdd8718a6b45150c0e42542e6aa8423d1c
+    REF 60af9bbd094d58a6531bd6537f04ae257062c543
+    SHA512 8649f808473fc73e337e3ef40c528274fe81543fd6d5e2f0866c7c8b43b90dc2dd031d816c176a2c5f4493af06eb02fc679684039151880ea04744c6d3a19f05
     HEAD_REF main
 )
 
 file(INSTALL
     DIRECTORY "${SOURCE_PATH}/dejavu-sans/"
+    DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}/fonts"
+    FILES_MATCHING
+    PATTERN "*.ttc"
+    PATTERN "*.ttf"
+    PATTERN "*.otf"
+    PATTERN "*.woff2"
+    PATTERN "*.txt"
+)
+
+file(INSTALL
+    DIRECTORY "${SOURCE_PATH}/source-han-sans/"
     DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}/fonts"
     FILES_MATCHING
     PATTERN "*.ttc"
@@ -31,6 +42,7 @@ file(INSTALL
 vcpkg_install_copyright(
     FILE_LIST
         ${SOURCE_PATH}/dejavu-sans/DejaVu-License.txt
+        ${SOURCE_PATH}/source-han-sans/SourceHanSans.txt
         ${SOURCE_PATH}/twemoji/Twemoji-MIT-license.txt
         ${SOURCE_PATH}/twemoji/Twemoji-Artwork-CC-BY-license.txt
     COMMENT "Fonts contained within this package are licensed as follows"
