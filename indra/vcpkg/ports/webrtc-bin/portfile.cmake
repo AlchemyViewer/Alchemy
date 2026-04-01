1595e1ea02364299779f2165a0f6a80e7d7ad389
Rye <rye@alchemyviewer.org>
2026-04-01 08:58:10 -0400

    Update webrtc-bin to fix windows CRT linkage and use native webrtc xz sdk packages

--- indra/vcpkg/ports/webrtc-bin/portfile.cmake
+++ indra/vcpkg/ports/webrtc-bin/portfile.cmake
@@ -7,44 +7,52 @@ if(VCPKG_TARGET_IS_WINDOWS)
 
     vcpkg_download_distfile(
         WEBRTC_ARCHIVE
-        URLS https://github.com/secondlife/3p-webrtc-build/releases/download/m137.7151.04.23/webrtc-m137.7151.04.23.22004231636-windows64-22004231636.tar.zst
-        FILENAME webrtc-windows64.tar.zst
-        SHA512 0b57ef8c863dd1e1c8c90bd9d63d93b57b4c690a8b30bc8417ad2cfac395d1ee6a6e7324470b5a220c96c2086446447119419300a66389a30285a2a73be5ff47
+        URLS https://github.com/AlchemyViewer/3p-webrtc/releases/download/m137.7151.04.20-r10/webrtc.windows_x86_64.tar.xz
+        FILENAME webrtc.windows_x86_64.tar.xz
+        SHA512 f4215881234c9d2b64ee7c21ed5d321952ab1de73e9511846a994d9cc4d76d7e4dccbd3c5e87e7d2068f9871bee7833669d4a0a90cce4482a3d79905cf40bc97
     )
 elseif(VCPKG_TARGET_IS_OSX)
     set(WEBRTC_LIBNAME "libwebrtc.a")
 
-    vcpkg_download_distfile(
-        WEBRTC_ARCHIVE
-        URLS https://github.com/secondlife/3p-webrtc-build/releases/download/m137.7151.04.23/webrtc-m137.7151.04.23.22004231636-darwin64-22004231636.tar.zst
-        FILENAME webrtc-osx.tar.zst
-        SHA512 84f7c91b8d92ea3037bbf4370a8142d97260916c2ca62dad7641317f6a3da66154e12926a213f3ab3c5d96c5fc468643fc8aa13bca348efd0a0c8f080a52545d
-    )
+    if(VCPKG_OSX_ARCHITECTURES MATCHES "arm64")
+        vcpkg_download_distfile(
+            WEBRTC_ARCHIVE
+            URLS https://github.com/AlchemyViewer/3p-webrtc/releases/download/m137.7151.04.20-r9/webrtc.macos_arm64.tar.xz
+            FILENAME webrtc.macos_arm64.tar.xz
+            SHA512 64f0dd819f33a446c480a56897ffb928abdaedb745bd021f1efed50f790ad3ffbefb7b1655ba6f91ac6f7490fb4d4f66892d1882e03a27077a1f4078ffc97ab5
+        )
+    else()
+        vcpkg_download_distfile(
+            WEBRTC_ARCHIVE
+            URLS https://github.com/AlchemyViewer/3p-webrtc/releases/download/m137.7151.04.20-r9/webrtc.macos_x86_64.tar.xz
+            FILENAME webrtc.macos_x86_64.tar.xz
+            SHA512 53e831d19c4bcd41145d7f183dfbe401da153dbc311ea8d26b3bdef146cb1349e7792a8b1f94098eebd88fe74801f465344167d6a8b06f99d06f01fa6286ea2e
+        )
+    endif()
 elseif(VCPKG_TARGET_IS_LINUX)
     set(WEBRTC_LIBNAME "libwebrtc.a")
 
     vcpkg_download_distfile(
         WEBRTC_ARCHIVE
-        URLS https://github.com/secondlife/3p-webrtc-build/releases/download/m137.7151.04.23/webrtc-m137.7151.04.23.22004231636-linux64-22004231636.tar.zst
-        FILENAME webrtc-linux64.tar.zst
-        SHA512 7f3a84ba9f6da66efe5ca1a475601df49bc0a80d8e9257765ff4a9720f07ccc6649d0421e5041ceaa322ea5d6059bbafcbc505b4cf7d13af22251fb76d799098
+        URLS https://github.com/AlchemyViewer/3p-webrtc/releases/download/m137.7151.04.20-r9/webrtc.ubuntu-22.04_x86_64.tar.xz
+        FILENAME webrtc.ubuntu-22.04_x86_64.tar.xz
+        SHA512 7a7c248929055b0026cf9362188848a4e5624c9ceb149ae3b2f4a27c2e15e41c9063ac20a87c2a6e140fd3ad5c46b010f12be9ab076f383ed871c534bf51403e
     )
 endif()
 
 vcpkg_extract_source_archive(
     WEBRTC_DIR
     ARCHIVE ${WEBRTC_ARCHIVE}
-    NO_REMOVE_ONE_LEVEL
 )
 
 file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/include/")
 file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/lib/")
-file(RENAME "${WEBRTC_DIR}/include/webrtc/" "${CURRENT_PACKAGES_DIR}/include/webrtc/")
+file(RENAME "${WEBRTC_DIR}/include/" "${CURRENT_PACKAGES_DIR}/include/webrtc/")
 if (VCPKG_TARGET_IS_LINUX)
     file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include/webrtc/build/linux/debian_bullseye_i386-sysroot")
     file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include/webrtc/build/linux/debian_bullseye_amd64-sysroot")
 endif()
 
-file(RENAME "${WEBRTC_DIR}/lib/release/${WEBRTC_LIBNAME}" "${CURRENT_PACKAGES_DIR}/lib/${WEBRTC_LIBNAME}")
+file(RENAME "${WEBRTC_DIR}/lib/${WEBRTC_LIBNAME}" "${CURRENT_PACKAGES_DIR}/lib/${WEBRTC_LIBNAME}")
 
-vcpkg_install_copyright(FILE_LIST ${WEBRTC_DIR}/LICENSES/webrtc-license.txt)
+vcpkg_install_copyright(FILE_LIST ${WEBRTC_DIR}/NOTICE)
