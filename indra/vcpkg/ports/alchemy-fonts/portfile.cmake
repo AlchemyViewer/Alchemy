2378b624b0eaaf9aeb6e76ed533ca47ae20eb5dd
Rye <rye@alchemyviewer.org>
2026-04-24 21:51:30 -0400

    Add updated fonts package with InterVariable

--- indra/vcpkg/ports/alchemy-fonts/portfile.cmake
+++ indra/vcpkg/ports/alchemy-fonts/portfile.cmake
@@ -3,9 +3,20 @@ set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO AlchemyViewer/alchemy-fonts
-    REF 60af9bbd094d58a6531bd6537f04ae257062c543
-    SHA512 8649f808473fc73e337e3ef40c528274fe81543fd6d5e2f0866c7c8b43b90dc2dd031d816c176a2c5f4493af06eb02fc679684039151880ea04744c6d3a19f05
+    REF ${VERSION}
+    SHA512 db35caf90f5eaf55e32bf266b00dbd1e1d1c2b7f80e6175c19342da1df4561a73735a35ef7f6e363b19d0d92e03069966a97b0764ca5bed277cdbf6f9f822475
     HEAD_REF main
+)
+
+file(INSTALL
+    DIRECTORY "${SOURCE_PATH}/inter/"
+    DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}/fonts"
+    FILES_MATCHING
+    PATTERN "*.ttc"
+    PATTERN "*.ttf"
+    PATTERN "*.otf"
+    PATTERN "*.woff2"
+    PATTERN "*.txt"
 )
 
 file(INSTALL
@@ -43,6 +54,7 @@ file(INSTALL
 
 vcpkg_install_copyright(
     FILE_LIST
+        ${SOURCE_PATH}/inter/Inter-License.txt
         ${SOURCE_PATH}/dejavu-sans/DejaVu-License.txt
         ${SOURCE_PATH}/source-han-sans/SourceHanSans.txt
         ${SOURCE_PATH}/twemoji/Twemoji-MIT-license.txt
