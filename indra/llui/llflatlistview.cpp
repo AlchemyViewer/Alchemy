3c8d3a6205db0606359d2d25dddf7e3973b0aaa3
gwigz <gwigz@users.noreply.github.com>
2026-06-14 17:33:24 +0100

    Harden layer reorder against unintended/partial changes

--- indra/llui/llflatlistview.cpp
+++ indra/llui/llflatlistview.cpp
@@ -687,7 +687,10 @@ void LLFlatListView::onItemMouseClick(item_pair_t* item_pair, MASK mask)
         setFocus(true);
     }
 
-    armReorderDrag(item_pair);
+    if (!(mask & (MASK_SHIFT | MASK_CONTROL)))
+    {
+        armReorderDrag(item_pair);
+    }
 
     bool select_item = !isSelected(item_pair);
 
