b8bbf34b2848cdcde819bce4bf3feb935cddcb75
Rye <rye@alchemyviewer.org>
2026-09-03 13:23:44 -0400

    Mitigate spammy calling card logging during login

--- indra/newview/llcallingcard.cpp
+++ indra/newview/llcallingcard.cpp
@@ -725,9 +725,8 @@ void LLAvatarTracker::processNotify(LLMessageSystem* msg, bool online)
             }
             else
             {
-                LL_WARNS() << "Received online notification for unknown buddy: "
-                    << agent_id << " is " << (online ? "ONLINE" : "OFFLINE") << LL_ENDL;
-                LL_WARNS() << "Adding buddy to buddy queue." << LL_ENDL;
+                LL_DEBUGS() << "Received online notification for unknown buddy: "
+                    << agent_id << " is " << (online ? "ONLINE" : "OFFLINE") << " Adding buddy to buddy queue." << LL_ENDL;
                 mBuddyStatusQueue.push(std::make_pair(agent_id, true));
             }
 
