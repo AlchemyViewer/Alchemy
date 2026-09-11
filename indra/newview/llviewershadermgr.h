96ff4ef8162360a3dc5e77f16687b891f91a13dd
Quinn Elara <128988672+Quinn-Elara@users.noreply.github.com>
2026-09-11 19:43:17 +0100

    Fix dof resolution divider applying to foreground items when foreground blur is disabled

--- indra/newview/llviewershadermgr.h
+++ indra/newview/llviewershadermgr.h
@@ -188,6 +188,7 @@ extern LLGLSLShader         gDeferredPostProgram;
 extern LLGLSLShader         gDeferredPostProgramNoNear;
 extern LLGLSLShader         gDeferredCoFProgram;
 extern LLGLSLShader         gDeferredDoFCombineProgram;
+extern LLGLSLShader         gDeferredDoFCombineProgramNoNear;
 extern LLGLSLShader         gFXAAProgram[4];
 extern LLGLSLShader         gSMAAEdgeDetectProgram[4];
 extern LLGLSLShader         gSMAABlendWeightsProgram[4];
